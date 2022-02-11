a2bd66f9eb24ec7389685c35f3d7b040e2ab5a86
mrescorcia <deg40@outlook.com>
2022-02-11 14:03:39 -0500

    We're privatizing parameters

--- OtiliaNet_ENT/Product_ENT.cs
+++ OtiliaNet_ENT/Product_ENT.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 namespace OtiliaNet.OtiliaNet_ENT
 {
     public class Product_ENT
-    {   
-        // This properties will be privatised
-        public int idProduct { get; } = 1;
-        public string nameProduct { get; set; }
-        public string descProduct { get; set; }
+    {
+        private string NameProduct;
+        private string DescProduct;
+
+        public int idProduct {
+            get => 1;
+        }
+        public string nameProduct 
+        { 
+            get => NameProduct;
+            set => NameProduct = value; 
+        }
+        public string descProduct 
+        {
+            get => DescProduct;  
+            set => DescProduct = value;
+        }
     }
 }
---
+++ src/Queries.sql
@@ -0,0 +1,2 @@
+insert into tests_Products (id_product,nameProduct,descProduct) values (1, 'Computador Dell 250GB','DDR4, SSD 250GB, Intel Core i9')
+select * from tests_Products
